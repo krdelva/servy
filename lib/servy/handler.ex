738ed786e91ceb48c35c19c126605317e13a4e72
krdelva <kevin.r.delva@gmail.com>
2022-10-11 16:35:18 -0400

    Fix typo in sensors.eex, comment out last line in router /sensors from handler

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -36,7 +36,7 @@ defmodule Servy.Handler do
     where_is_bigfoot = Task.await(task)
 
     render(conv, "sensors.eex", snapshots: snapshots, location: where_is_bigfoot)
-    %{ conv | status: 200, resp_body: inspect {snapshots, where_is_bigfoot} }
+    #%{ conv | status: 200, resp_body: inspect {snapshots, where_is_bigfoot} }
   end
 
   def route(%Conv{ method: "GET", path: "/kaboom"} = _conv) do
