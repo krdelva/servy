131bd9ae85af2db51d3de2c8f1ed3ce2793cb08d
krdelva <kevin.r.delva@gmail.com>
2022-10-14 10:08:22 -0400

    Transform FourOhFourCounter to use GenServer instead of handwritten version

--- lib/servy/four_oh_four_counter.ex
+++ lib/servy/four_oh_four_counter.ex
@@ -1,43 +1,43 @@
-defmodule Servy.FourOhFourCounter.GenericServer do
+# defmodule Servy.FourOhFourCounter.GenericServer do
 
-  def start(callback_module, initial_state, name) do
-    IO.puts "Starting the 404 counter..."
-    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
-    Process.register(pid, name)
-    pid
-  end
-
-
-
-  # Helper Function
-
-  def call(pid, message) do
-    send pid, {:call, self(), message}
-
-    receive do {:response, response} -> response end
-  end
-
-  def cast(pid, message) do
-    send pid, {:cast, message}
-  end
-
-  def listen_loop(state, callback_module) do
-    receive do
-      {:call, sender, message} ->
-        {response, new_state} = callback_module.handle_call(message, state)
-        send sender, {:response, response}
-        listen_loop(new_state, callback_module)
-      {:cast, message} ->
-        new_state = callback_module.handle_cast(message, state)
-        listen_loop(new_state, callback_module)
-      unexpected ->
-        IO.puts "Unexpected message: #{inspect unexpected}"
-        listen_loop(state, callback_module)
-    end
-  end
+#   def start(callback_module, initial_state, name) do
+#     IO.puts "Starting the 404 counter..."
+#     pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
+#     Process.register(pid, name)
+#     pid
+#   end
 
 
-end
+
+#   # Helper Function
+
+#   def call(pid, message) do
+#     send pid, {:call, self(), message}
+
+#     receive do {:response, response} -> response end
+#   end
+
+#   def cast(pid, message) do
+#     send pid, {:cast, message}
+#   end
+
+#   def listen_loop(state, callback_module) do
+#     receive do
+#       {:call, sender, message} ->
+#         {response, new_state} = callback_module.handle_call(message, state)
+#         send sender, {:response, response}
+#         listen_loop(new_state, callback_module)
+#       {:cast, message} ->
+#         new_state = callback_module.handle_cast(message, state)
+#         listen_loop(new_state, callback_module)
+#       unexpected ->
+#         IO.puts "Unexpected message: #{inspect unexpected}"
+#         listen_loop(state, callback_module)
+#     end
+#   end
+
+
+# end
 
 
 defmodule Servy.FourOhFourCounter do
@@ -46,48 +46,52 @@ defmodule Servy.FourOhFourCounter do
   #@name __MODULE__
   # Client Interface
 
-  alias Servy.FourOhFourCounter.GenericServer
+  #alias Servy.FourOhFourCounter.GenericServer
+  use GenServer
 
   def start do
     IO.puts "Starting the 404 counter..."
-    GenericServer.start(__MODULE__, %{}, @name)
+    GenServer.start(__MODULE__, %{}, name: @name)
   end
 
   def bump_count(path) do
-    GenericServer.call @name, {:bump_count, path}
+    GenServer.call @name, {:bump_count, path}
   end
 
   def get_counts do
-    GenericServer.call @name, :get_counts
+    GenServer.call @name, :get_counts
   end
 
   def get_count(path) do
-    GenericServer.call @name, {:get_count, path}
+    GenServer.call @name, {:get_count, path}
   end
 
   def reset do
-    GenericServer.cast @name, :reset
+    GenServer.cast @name, :reset
   end
 
 
   # Server Callbacks
 
+  def init(state) do
+    {:ok, state}
+  end
 
-  def handle_call({:bump_count, path}, state ) do
+  def handle_call({:bump_count, path}, _from, state ) do
     new_state = Map.update(state, path, 1, &(&1 + 1))
-    {:ok, new_state}
+    {:reply, :ok, new_state}
   end
 
-  def handle_call(:get_counts, state) do
-    {state, state}
+  def handle_call(:get_counts, _from, state) do
+    {:reply, state, state}
   end
 
-  def handle_call({:get_count, path}, state) do
+  def handle_call({:get_count, path}, _from, state) do
     count = Map.get(state, path, 0)
-    {count, state}
+    {:reply, count, state}
   end
 
   def handle_cast(:reset, _state) do
-    %{}
+    {:noreply, %{}}
   end
 end
